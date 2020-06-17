a892aa0c2ca497bf35088c23021c18e28d6cd220
Thomas <thomas@kayser.com>
2020-06-17 21:46:17 +0200

    Removed PLN currency from TickerContract default

--- src/main/java/com/coinpaprika/apiclient/repository/ticker/TickerApiContract.kt
+++ src/main/java/com/coinpaprika/apiclient/repository/ticker/TickerApiContract.kt
@@ -1,7 +1,6 @@
 package com.coinpaprika.apiclient.repository.ticker
 
 import com.coinpaprika.apiclient.entity.TickerEntity
-import io.reactivex.Observable
 import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Path
@@ -9,7 +8,7 @@ import retrofit2.http.Query
 
 internal interface TickerApiContract {
     @GET("tickers/{id}/")
-    suspend fun getTicker(@Path("id") id: String, @Query("quotes") quotes: String = "USD,BTC,ETH,PLN"): Response<TickerEntity>
+    suspend fun getTicker(@Path("id") id: String, @Query("quotes") quotes: String = "USD,BTC,ETH"): Response<TickerEntity>
 
     @GET("tickers")
     suspend fun getTickers(@Query("quotes") quotes: String = "USD,BTC,ETH"): Response<List<TickerEntity>>
